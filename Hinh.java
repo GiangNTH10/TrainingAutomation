9e12da34d0baed6d80f1a28d01e0008b68b5fc05
annguyen1710 <nguyenttan1097@gmail.com>
2018-09-12 23:39:35 +0700

    Add Hinh.java

---
+++ Hinh.java
@@ -0,0 +1,40 @@
+package HinhHoc;
+
+public abstract class Hinh {
+	private double canh1;
+	private double canh2;
+
+	public double getCanh1() {
+		return canh1;
+	}
+
+	public void setCanh1(double canh) {
+		this.canh1 = canh;
+	}
+
+	public double getCanh2() {
+		return canh2;
+	}
+
+	public void setCanh2(double canh) {
+		this.canh2 = canh;
+	}
+
+	public double chuvi() {
+		double cv = 0;
+		return cv;
+	}
+
+	public double dientich() {
+		double cv = 0;
+		return cv;
+	}
+
+	public void khoitaodulieu() {
+	};
+
+	public void display(double giatri) {
+		System.out.println(" " + giatri);
+	}
+
+}
